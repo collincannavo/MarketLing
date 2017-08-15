a89f984e210d65d6ac89f9b9d252efb60c457a19
Bop <collin.cannavo@gmail.com>
2017-08-14 21:41:17 -0600

    Added previousMonth collection view

---
+++ MarketLing/PreviousMonthCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PreviousMonthCollectionViewCell.swift
+//  MarketLing
+//
+//  Created by Collin Cannavo on 8/14/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+
+class PreviousMonthCollectionViewCell: UICollectionViewCell {
+    
+}
