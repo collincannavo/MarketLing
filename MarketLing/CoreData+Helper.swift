9f5b886665dc0b33172f42c006620bdb2a1336f1
Bop <collin.cannavo@gmail.com>
2017-08-14 21:14:54 -0600

    Added core data for source model

---
+++ MarketLing/CoreData+Helper.swift
@@ -0,0 +1,27 @@
+//
+//  CoreData+Helper.swift
+//  MarketLing
+//
+//  Created by Collin Cannavo on 8/14/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
